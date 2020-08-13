56b31e63ac898c240b2dae5e5f7d91d331a3cf03
Nathan Bahia <nathanbabahia@gmail.com>
2020-08-13 09:28:49 -0300

    Cria M V T  para cadastro de matérias close #1

--- registros/models.py
+++ registros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class MateriaModel(models.Model):
+    materia = models.CharField('Matéria', max_length=100)
+
+    class Meta: 
+        verbose_name = 'Matéria'
+        verbose_name_plural = 'Matérias'
+
+    def __str__(self):
+        return self.materia
