5cffa56f89bea0014927f21d4c56c375ad57ad2f
Nathan Bahia <nathanbabahia@gmail.com>
2020-08-13 17:01:52 -0300

    Cria M V T para registros close #4

--- registros/models.py
+++ registros/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class MateriaModel(models.Model):
@@ -10,3 +11,15 @@ class MateriaModel(models.Model):
 
     def __str__(self):
         return self.materia
+
+class RegistroModel(models.Model):
+    data = models.DateField('Data', default=datetime.now())
+    materia = models.ForeignKey('MateriaModel', on_delete=models.CASCADE)
+    registro = models.TextField('Registro', max_length=2000)
+
+    class Meta:
+        verbose_name = 'Registro'
+        verbose_name_plural = 'Registros'
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.data, self.materia, self.registro[:50]+'...')
--- registros/views.py
+++ registros/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import MateriaModelForm
-from .models import MateriaModel
+from .forms import MateriaModelForm, RegistroModelForm
+from .models import MateriaModel, RegistroModel
+from datetime import datetime
 
 
 def index(request):
     """ Renderiza o template da página inicial """
-    return render(request, 'index.html')
+
+    form = RegistroModelForm(request.POST or None)
+    if str(request.method) == 'POST':        
+        if form.is_valid():                      
+            form.save()
+            form = RegistroModelForm()
+            messages.success(request, 'Registro inserido com sucesso.')
+        else:
+            messages.error(request, 'Falha ao salvar registro.')
+
+    materias = MateriaModel.objects.all()
+    registros = RegistroModel.objects.all()
+    context = {'form': form, 'materias': materias, 'registros': registros}
+    return render(request, 'index.html', context)
 
 
 def materias(request):
@@ -16,7 +30,6 @@ def materias(request):
     no banco de dados antes de salvar. Exibe uma mensagem de 
     sucesso ou erro ao fim do processo.
      """
-
 
     form = MateriaModelForm(request.POST or None)
     if str(request.method) == 'POST':
